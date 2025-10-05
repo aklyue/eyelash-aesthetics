898afd692bbc25cd12ae5610e18e2f897c1573ba
unknown <sryitscool59@gmail.com>
2025-10-05 17:15:47 +0700

    Hotfixed bookings api

--- src/store/slices/bookedDatesSlice.ts
+++ src/store/slices/bookedDatesSlice.ts
@@ -14,7 +14,7 @@ export const loadBookedDates = createAsyncThunk(
   "bookedDates/load",
   async () => {
     const res = await fetch(
-      `http://localhost:3001/bookedDates`
+      `https://eyelash-aesthetics-api.onrender.com/bookedDates`
     );
     const data = await res.json();
     return data.map(
