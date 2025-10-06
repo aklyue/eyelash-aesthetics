e621bc9073736838de24e90961ec15f19b5c7458
unknown <sryitscool59@gmail.com>
2025-10-06 16:51:28 +0700

    Fixed service selection styles

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -186,7 +186,7 @@ export default function Contacts() {
                         "& .Mui-selected": {
                           bgcolor: "#ce92d71a",
                           "&:hover": {
-                            bgcolor: isMobile ? "rgba(0,0,0,0)" : "#ce92d738",
+                            bgcolor: isMobile ? "#ce92d71a" : "#ce92d738",
                           },
                         },
                       }}
